bb4b8e20d4460429b1cd3124be8727237579c0ac
eltroneur <cherishdevice@163.com>
2019-06-04 15:22:11 +0800

    using cin.get().get()

---
+++ instr3.cpp
@@ -0,0 +1,17 @@
+// instr3.cpp -- reading more than one word with get() & get()
+#include <iostream>
+int main()
+{
+  const int ArSize = 20;
+  char name[ArSize];
+  char dessert[ArSize];
+
+  std::cout << "Enter your name:\n";
+  std::cin.get(name, ArSize).get(); // read string, newline
+  std::cout << "Enter your favorite dessert:\n";
+  std::cin.get(dessert, ArSize).get();
+  std::cout << "I have some delicious " << dessert ;
+  std::cout << " for you, " << name << ".\n";
+
+  return 0;
+}
