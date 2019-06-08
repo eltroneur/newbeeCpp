f02546e828eaa6c8c468a33d83c082a81a4eb656
eltroneur <cherishdevice@163.com>
2019-06-09 04:02:04 +0800

    add comments of arrayInit.cpp

--- arrayInit.cpp
+++ arrayInit.cpp
@@ -4,10 +4,11 @@
 
 int main()
 {
-  int arr[3] = {1};
-  //getchar();
+  int arr[3] = {1};  // first elemet of arr is 1, the rest set to 0
   int newArr[4] {2, 5, 8, 1};
 
-  unsigned int counts[10] = {25, 92, 3.0};
-  unsigned int debets[10] {};
+  unsigned int counts[10] = {25, 92, 3.0};  // (Warning:) narrowing conversion from double to unsigned int
+  unsigned int debets[10] {};  // all undefined values
+
+  return 0;
 }
