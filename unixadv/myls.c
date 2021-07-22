115b66c90d4c7c837f3e41a3ce95d6e65fd62fdd
magpad <magpad@sb>
2021-07-22 23:22:05 +0800

    1.adding directory "unixadv" to track the code of "Advance Programming Unix..." 2.adding the first example of "AdvUnixProgram" -- reading directory.

---
+++ unixadv/myls.c
@@ -0,0 +1,34 @@
+// myls.cpp
+#include <stdio.h>
+#include <sys/types.h>
+#include <dirent.h>
+#include <stdlib.h>
+#include <errno.h>
+
+int main(int argc, char *argv[])
+{
+  if (argc != 2)
+  {
+    printf("usage: %s dirname\n", argv[0]);
+    exit(-1);
+  }
+
+  DIR *objDir = NULL;
+  objDir = opendir(argv[1]);
+  if (NULL == objDir)
+  {
+    int iErr = errno;
+    perror("Err when opening directory");
+    exit(-1);
+  }
+
+  struct dirent *subItem = NULL;
+  while ( (subItem = readdir(objDir)) )
+  {
+    printf("%s ", subItem->d_name);
+  }
+
+  printf("\n");
+
+  return 0;
+}
